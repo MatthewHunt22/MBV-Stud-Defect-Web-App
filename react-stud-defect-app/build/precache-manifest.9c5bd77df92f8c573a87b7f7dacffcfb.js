a635864961428c6dcffc6e865c72a43bf5f2ab96
MatthewHunt22 <56851398+MatthewHunt22@users.noreply.github.com>
2020-04-28 14:13:39 -0400

    Add files via upload

---
+++ react-stud-defect-app/build/precache-manifest.9c5bd77df92f8c573a87b7f7dacffcfb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78651d902c849f9be850c6133f97f252",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8711e6999649806c431e",
+    "url": "/static/css/main.d99ae5b9.chunk.css"
+  },
+  {
+    "revision": "6ec7eb2a0974feebca15",
+    "url": "/static/js/2.873b6778.chunk.js"
+  },
+  {
+    "revision": "e0ad3387081147deea4b08b0e0fff77f",
+    "url": "/static/js/2.873b6778.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8711e6999649806c431e",
+    "url": "/static/js/main.d22f0e62.chunk.js"
+  },
+  {
+    "revision": "02dafd89d80bb0374c02",
+    "url": "/static/js/runtime-main.adf88448.js"
+  },
+  {
+    "revision": "3f320178ba1dc2767de038487f94faf2",
+    "url": "/static/media/daimler.3f320178.png"
+  },
+  {
+    "revision": "4878aaf70c3eb656fdef259af916ae51",
+    "url": "/static/media/mbvstar.4878aaf7.png"
+  }
+]);
